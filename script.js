a3c911aae13883543e50a475ba3ae6767f68b51b
Farhan <farhan.sajithara@gmail.com>
2026-05-24 01:44:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Vivaan Baby Care Loaded");
+const supabaseUrl =
+"https://enurllcrzipslevvmitw.supabase.co";
+
+const supabaseKey =
+"sb_publishable_mTOKUPDyB8aFCOzsd5zYaA_DmDRUXFi";
+
+const supabaseClient =
+supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+async function loadProducts() {
+
+  const { data, error } =
+  await supabaseClient
+  .from("products")
+  .select("*");
+
+  console.log(data);
+
+}
+
+loadProducts();
